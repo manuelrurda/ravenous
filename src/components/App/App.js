1e829f84c6fa54efc85e3969981938da835bd13d
manuelrurda <manuelrodriguezurda@Manuels-MacBook-Air.local>
2022-10-17 12:17:48 -0500

    Search bar and render in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,18 @@
-import logo from "../../logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar></SearchBar>
+        <BusinessList></BusinessList>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Business from "../Business/Business";
+import "./BusinessList.css";
 
 class BusinessList extends React.Component {
   render() {
