f3a76f441b47938c6a3fa5c2f2942ee0b4afef5d
manuelrurda <manuelrodriguezurda@Manuels-MacBook-Air.local>
2022-10-18 18:22:02 -0500

    Yelp api util file

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,34 @@
 const apiKey = `${process.env.REACT_APP_YELP_API_KEY}`;
+
+const Yelp = {};
+
+const generateEndpoint = (term, location, sortBy) => {
+  return `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+};
+
+const search = (term, location, sortBy) => {
+  return fetch(generateEndpoint(term, location, sortBy), {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  })
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Request failed");
+      },
+      (networkError) => console.log(networkError)
+    )
+    .then((jsonResponse) => {
+      if (jsonResponse.businesses) {
+        console.log(jsonResponse);
+        return jsonResponse.businesses.map((business) => {
+          return {};
+        });
+      }
+    });
+};
+
+export { search };
